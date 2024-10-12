7bbf8a7669697944ba269ae8c889c65a65ef5d09
sramesh621 <ramesh.srigiriraju@gmail.com>
2024-10-12 11:43:12 -0400

    Update PatientScheduler.h

--- PatientScheduler.h
+++ PatientScheduler.h
@@ -8,6 +8,9 @@ private:
     vector<Patient> patientList;
 public:
     PatientScheduler();
+    const Patient& hasPatient(const Patient& patient);
+    bool addPatient(const Patient& addMe);
+    bool removePatient(const Patient& removeMe);
     PatientScheduler(const PatientScheduler& original);
     const PatientScheduler& operator=(const& PatientScheduler copy);
     ~PatientScheduler();
