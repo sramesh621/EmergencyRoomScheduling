6dc62ccd4c243c933ed9ab3ee887d7d7b5bc33e5
sramesh621 <ramesh.srigiriraju@gmail.com>
2024-11-29 16:57:56 -0500

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -17,3 +17,13 @@ void Patient::setName(const string& newName)
 {
     name = newName;
 }
+
+const int Patient::getTimeToLive()
+{
+    return timeToLive;
+}
+
+void Patient::setTimeToLive(const int newTimeToLive)
+{
+    timeToLive = newTimeToLive;
+}
